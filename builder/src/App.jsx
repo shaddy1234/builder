25e9d77d1fd5320e5c8a6337a0459eb9b3ff7c68
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-01-30 11:49:44 +0300

    Raise state

--- builder/src/App.jsx
+++ builder/src/App.jsx
@@ -3,19 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/Login'
+import Updater from './components/Updater'
 
 function App() {
-  const [formData, setFormData] = useState({
-    firstName:'',
-    lastName:'',
-    email:'',
-    password:'',
-    confirmPassword:''
-  })
+   const [formData, setFormData] = useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:'',
+        confirmPassword:''
+      })
 
+    function handleChange(e) {
+        setFormData(prevFormData => ({
+          ...prevFormData,
+          [e.target.name]: e.target.value
+        }));
+        console.log(formData)
+      }
+      function handleSubmit(e) {
+        e.preventDefault()
+      }
   return (
     <>
-      <Login />
+    <div className='two'>
+      <Login formData={formData} handleChange={handleChange} handleSubmit={handleSubmit}/>
+      <Updater formData={formData}/>
+    </div>
+    
     </>
   )
 }
